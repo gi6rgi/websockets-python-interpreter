a1e9b867e081f1d11a6f483c8b9e2b80e85b9c48
ShredsGang <goshashreeeds@gmail.com>
2021-01-17 20:22:44 +0300

    server

---
+++ websockets_server.py
@@ -0,0 +1,17 @@
+import websockets
+import asyncio
+
+
+class CodeExecutor:
+
+    async def _stream_transfer(self):
+        pass
+
+    async def execute_code(self):
+        pass
+
+
+class Server(CodeExecutor):
+
+    async def connectin_handler(self):
+        pass
