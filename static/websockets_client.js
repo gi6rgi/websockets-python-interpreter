d6bedda33092d4db9547b81f6b0e2b269c745499
ShredsGang <goshashreeeds@gmail.com>
2021-01-18 02:08:02 +0300

    view layer

---
+++ static/websockets_client.js
@@ -0,0 +1,33 @@
+var left_editor = CodeMirror.fromTextArea(document.getElementById("left_terminal"), {
+  lineNumbers: true,
+});
+
+var right_editor = CodeMirror.fromTextArea(document.getElementById("right_terminal"), {
+  lineNumbers: true,
+  readOnly: true,
+  cursorBlinkRate: 0,
+});
+
+const ws = new WebSocket("ws://localhost:3000");
+// Some inkostilation happens here.
+curr_line = '';
+
+function sendCode() {
+  curr_line = '';
+  code = left_editor.getValue();
+  ws.send(code);
+};
+
+ws.onmessage = msg => {
+  curr_line += msg.data + '\n';
+  right_editor.setValue(curr_line);
+};
+
+ws.onclose = function(event) {
+  if (event.wasClean) {
+    right_editor.setValue('The connection was closed.');
+  } else {
+    right_editor.setValue('Сonnection drop-out was detected.'); // например, "убит" процесс сервера
+  }
+  right_editor.setValue('Code: ' + event.code + ' reason: ' + event.reason);
+};
