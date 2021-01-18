b95d0343b9042186628d9afafb64df330072b9e6
ShredsGang <goshashreeeds@gmail.com>
2021-01-18 17:19:06 +0300

    rewrited timeout

--- websockets_server.py
+++ websockets_server.py
@@ -1,6 +1,8 @@
 import websockets
 import asyncio
 import logging
+import sys
+from config import TIMEOUT
 from datetime import datetime
 from websockets import WebSocketClientProtocol
 from asyncio.subprocess import Process
@@ -11,13 +13,16 @@ logging.basicConfig(filename='log', level=logging.INFO)
 
 class CodeExecutor:
 
+    def __init__(self, timeout: int):
+        self.timeout = timeout
+
     @staticmethod
-    async def _stream_transfer(proc: Process, ws: WebSocketClientProtocol) -> None:
-        """Processes stout/err from the process pipe;
-        Sends realtime output through the websocket.
+    async def _stream_transfer(process: Process, ws: WebSocketClientProtocol) -> None:
+        """Captures stdout/err from the process pipe;
+        Sends output data through the websocket.
         """
         while True:
-            output = await proc.stdout.readline()
+            output = await process.stdout.readline()
             output = output.decode()
             if output:
                 await ws.send(output[:-1])
@@ -32,34 +37,44 @@ class CodeExecutor:
         Provides realtime stdout stream.
         """
         sanitized_input = f'import sanitizer.sanitizer\n{code_text}'
-        proc = await asyncio.create_subprocess_exec(
+        process = await asyncio.create_subprocess_exec(
             'python3', '-u', '-c', sanitized_input,
             stdin=asyncio.subprocess.PIPE,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.STDOUT
         )
-        await self._stream_transfer(proc, ws)
+        try:
+            await asyncio.wait_for(self._stream_transfer(process, ws), self.timeout)
+        except asyncio.TimeoutError:
+            await ws.send('TimeoutError')
 
 
 class Server(CodeExecutor):
 
+    def __init__(self, timeout: int):
+        super().__init__(timeout)
+
     @staticmethod
-    async def code_logger(code_text: str, ws: WebSocketClientProtocol) -> None:
-        """User input logger."""
+    def _get_server_time():
         now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        return now.strftime("%d/%m/%Y %H:%M:%S")
+
+    async def _code_logger(self, code_text: str, ws: WebSocketClientProtocol) -> None:
+        """User input logger."""
+        dt_string = self._get_server_time()
         logging.info(f'{dt_string}\n'
                      f'{ws.remote_address}\n'
                      f'{code_text}\n')
 
     async def connectin_handler(self, ws: WebSocketClientProtocol, uri: str) -> None:
+        await ws.send(sys.version)
         async for code_text in ws:
             await self.execute_code(code_text, ws)
-            await self.code_logger(code_text, ws)
+            await self._code_logger(code_text, ws)
 
 
 if __name__ == '__main__':
-    server = Server()
-    start_server = websockets.serve(server.connectin_handler, 'localhost', 3000)
+    server = Server(TIMEOUT)
+    start_server = websockets.serve(server.connectin_handler, '0.0.0.0', 3000)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
