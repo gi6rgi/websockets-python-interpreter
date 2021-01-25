2830646147140f281422b5d322104552ee1e0640
shredsgang <goshashreeeds@gmail.com>
2021-01-26 01:09:16 +0300

    sanitizer vuln fix

--- websockets_server/sanitizer/sanitizer.py
+++ websockets_server/sanitizer/sanitizer.py
@@ -1,16 +1,16 @@
 import builtins
 
 
-builtins.open  = lambda *args, *kwargs: print('Built-in function "open" is not allowed.')
-builtins.exec  = lambda *args, *kwargs: print('Built-in function "exec" is not allowed.')
-builtins.eval  = lambda *args, *kwargs: print('Built-in function "eval" is not allowed.')
-builtins.input = lambda *args, *kwargs: print('Built-in function "input" is not allowed.')
+builtins.open  = lambda *args, **kwargs: print('Built-in function "open" is not allowed.')
+builtins.exec  = lambda *args, **kwargs: print('Built-in function "exec" is not allowed.')
+builtins.eval  = lambda *args, **kwargs: print('Built-in function "eval" is not allowed.')
+builtins.input = lambda *args, **kwargs: print('Built-in function "input" is not allowed.')
 
 new_import = builtins.__import__
 
 
 def safe_import(module_name, *args, **kwargs):
-    restricted_modules = ['socket', 'os', 'subprocess', 'sys']
+    restricted_modules = ['importlib', 'socket', 'os', 'subprocess', 'sys']
     if module_name in restricted_modules:
         print(f'{module_name} is not allowed here.')
     else:
